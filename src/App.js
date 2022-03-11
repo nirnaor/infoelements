ebdc77a2265af0919c0b2d2ca2a1a684aeb98292
Nir Naor <nirnaori@gmail.com>
2022-03-11 09:40:05 +0200

    add config remove aligner.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import './App.css';
+import config from './config'
+console.log(config)
 
 function App() {
   return (
     <div className="parent">
       <div className="section top"> 
-        <div className="aligner title">
-            Licorice
+        <div className="title">
+          Licorice
         </div>
       </div>
       <div className="section middle">
-        <div className="aligner description">
-            nir
+        <div className="description">
+          A popular herbal ingridient in colds and flu teas
+          a widespread
         </div>
-        <div className="aligner icons">
-          <div className="icons">
-            <a href="#" className="fa fa-facebook"></a>
-            <a href="#" className="fa fa-twitter"></a>
-          </div>
+        <div className="icons">
+          <a href="#" className="fa fa-facebook"></a>
+          <a href="#" className="fa fa-twitter"></a>
         </div>
       </div>
 
       <div className="section bottom">
-        <div className="aligner">
-          <div className="custombutton">
-            <input type="button" value="Button" />
-          </div>
+        <div className="custombutton">
+          <input type="button" value="Button" />
         </div>
       </div>
     </div>
---
+++ src/config.js
@@ -0,0 +1,70 @@
+const config = {
+  internal: {
+    order: {
+      top: ['Title', 'Description'],
+      middle: ['Icons'],
+      bottom: ['CustomButton']
+    },
+    enabled: {
+      Title: true
+    },
+    titleParams: {
+      horizontalAlignment: "RIGHT",
+      textVerticalPadding: 3,
+      textHorizontalPadding: 5
+    },
+    descriptionParams: {
+      horizontalAlignment: "LEFT",
+      textVerticalPadding: 3,
+      textHorizontalPadding: 5
+    },
+    buttonParams: {
+      horizontalAlignment: "CENTER",
+      borderWidth: 3,
+      borderColor: "red"
+    },
+    iconsParams: {
+      enabled: {
+        download: false,
+        social: true,
+        heart: true
+      },
+      horizontalAlignment: "SPLIT"
+    }
+  },
+  external: {
+    order: {
+      top: [],
+      middle: [],
+      bottom: ['CustomButton']
+    },
+    enabled: {
+      CustomButton: true
+    },
+    titleParams: {
+      horizontalAlignment: "RIGHT",
+      textVerticalPadding: 3,
+      textHorizontalPadding: 5
+    },
+    descriptionParams: {
+      horizontalAlignment: "LEFT",
+      textVerticalPadding: 3,
+      textHorizontalPadding: 5
+    },
+    buttonParams: {
+      horizontalAlignment: "CENTER",
+      borderWidth: 3,
+      borderColor: "red"
+    },
+    iconsParams: {
+      enabled: {
+        download: false,
+        social: true,
+        heart: true
+      },
+      horizontalAlignment: "SPLIT"
+    }
+  },
+  externalPlacement: "ABOVE_IMAGE"
+}
+export default config
